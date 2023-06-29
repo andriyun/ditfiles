3a35b224bdc3db43e11f1298311614f1b232d6da
Andríyun <ayu@danskecommodities.com>
2023-06-29 08:51:04 +0200

    Added NIX aliaes

--- aliases.sh
+++ aliases.sh
@@ -20,3 +20,6 @@ alias gs='git status'
 alias gdc='git diff --cached'
 alias gca='git commit --amend'
 alias gp='git push'
+
+# Nix aliases
+alias ns='nix-shell --command zsh'
--- nix.sh
+++ nix.sh
@@ -1,2 +1,4 @@
 eval "$(direnv hook zsh)"
 if [ -e $HOME/.nix-profile/etc/profile.d/nix.sh ]; then . $HOME/.nix-profile/etc/profile.d/nix.sh; fi # added by
+
+# See NIX aliases in separate aliaes.sh file.
